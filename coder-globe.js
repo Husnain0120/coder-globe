2b403cc26d69faf6e5a557da07c42a330d647ddb
Husnain0120 <muhammadhusnainrashid.bs@gmail.com>
2024-01-07 12:10:16 +0500

    first commit

---
+++ coder-globe.js
@@ -0,0 +1,17 @@
+// function sub(){
+//     alert("Thanks for submit your form")
+// }
+
+// function scroll(){
+//     var a;
+//     a.window.scrollBy(0, 20);
+//     console.log(a);
+    
+// }
+$(document).ready(function(){
+$('#emailForm').submit(function () { 
+ 
+ alert("thank you ")
+    
+   });
+});
